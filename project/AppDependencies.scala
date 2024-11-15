1e0948333e3c9b75a3fce564cb801796988f2767
Paul Callaghan <83694362+PaulC-TDS@users.noreply.github.com>
2024-11-15 12:04:03 +0000

    DDCE-5900: Plug-in and dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import sbt.*
 object AppDependencies {
 
   val boostrapVersion = "9.5.0"
-  val mongoVersion = "1.9.0"
+  val mongoVersion = "2.3.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % mongoVersion,
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % boostrapVersion,
     "com.github.java-json-tools"    %  "json-schema-validator"      % "2.2.14",
-    "uk.gov.hmrc"                   %% "tax-year"                   % "3.3.0",
+    "uk.gov.hmrc"                   %% "tax-year"                   % "5.0.0",
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.18.1"
   )
 
