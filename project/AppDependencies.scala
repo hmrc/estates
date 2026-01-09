25076da5c41eb6f4de939a852f5eb2bb9acbe14e
Harish Korada <174133905+harish-korada1@users.noreply.github.com>
2026-01-09 12:21:27 +0000

    DDCE-7807: [Estates]: Align License Headers for Estates Service Repos (#82)
    * DDCE-7807: [Estates]: Align License Headers for Estates Service Repos
    
    * remove duplicate license headers
    
    * update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,15 +2,15 @@ import sbt.*
 
 object AppDependencies {
 
-  val boostrapVersion = "10.2.0"
-  val mongoVersion = "2.9.0"
+  val boostrapVersion = "10.5.0"
+  val mongoVersion = "2.11.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % mongoVersion,
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % boostrapVersion,
     "com.github.java-json-tools"    %  "json-schema-validator"      % "2.2.14",
     "uk.gov.hmrc"                   %% "tax-year"                   % "6.0.0",
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.20.0"
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.20.1"
   )
 
   val test: Seq[ModuleID] = Seq(
